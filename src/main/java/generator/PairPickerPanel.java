a31de915e3126f9cf1fa40398c0483bb18414d29
RjRDRG <rj.ribeiro@campus.fct.unl.pt>
2022-04-10 21:32:33 +0100

    v0.2

--- src/main/java/generator/PairPickerPanel.java
+++ src/main/java/generator/PairPickerPanel.java
@@ -1,13 +1,11 @@
 package generator;
 
 import generator.ui.*;
-import org.apache.commons.lang3.tuple.Pair;
 
 import javax.swing.*;
 import javax.swing.table.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
 import java.util.*;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,7 +27,7 @@ public class PairPickerPanel extends JPanel {
         final JList<String> ls0 = new JList<>();       final JList<String> ls1 = new JList<>();
         final JButton bt0 = new JButton();
 
-        final PairTable t0 = new PairTable();
+        final JMultiTable t0 = new JMultiTable();
 
         //--------------------------------------------------------------------------------------------------------------
 
@@ -116,7 +114,7 @@ public class PairPickerPanel extends JPanel {
             }
         };
 
-        t0.buildTable(columnNames, new HashSet<>(responses), removeRow);
+        t0.buildTable(columnNames, Set.of(0,1), removeRow);
 
         for(Map.Entry<String,Set<String>> entry : pairs.entrySet()) {
             buildRow(t0, entry.getKey(), entry.getKey(), entry.getValue(), entry.getValue());
@@ -131,7 +129,7 @@ public class PairPickerPanel extends JPanel {
 
     }
 
-    private void buildRow(PairTable table, String endpoint, String previousEndpoint, Set<String> responses, Set<String> responseOptions) {
+    private void buildRow(JMultiTable table, String endpoint, String previousEndpoint, Set<String> responses, Set<String> responseOptions) {
         String[] row = new String[table.getColumnCount()];
         row[0] = endpoint;
         row[1] = previousEndpoint;
@@ -141,107 +139,15 @@ public class PairPickerPanel extends JPanel {
             if(responses.contains(columnName)) {
                 if(responseOptions.contains(columnName))
                     row[i] = columnName;
+                else
+                    row[i] = "MISSING";
             }
+            else
+                row[i] = JMultiTable.BLANK;
         }
 
         row[table.getColumnCount()-1]="X";
 
-        table.getModel().addRow(row,responseOptions);
-    }
-}
-
-class PairTable extends JTable {
-
-    private final MultiTableModel model;
-
-    PairTable() {
-        super();
-        model = new MultiTableModel();
-    }
-
-    @Override
-    public TableCellEditor getCellEditor(int row, int column) {
-        String[] options = model.getOptions(row, column);
-        if (options != null) {
-            JComboBox<String> comboBox = new JComboBox<>(options);
-            comboBox.addActionListener((e)-> model.setValueAt(comboBox.getSelectedItem(), row, column));
-            return new DefaultCellEditor(comboBox);
-        } else {
-            return super.getCellEditor(row, column);
-        }
-    }
-
-    void buildTable(String[] columnNames, Set<String> columnWithOptions, Action additionalRemoveRowAction) {
-        model.setColumns(columnNames,columnWithOptions);
-        setModel(model);
-
-        getTableHeader().setReorderingAllowed(false);
-        getTableHeader().setResizingAllowed(false);
-        setCellSelectionEnabled(false);
-        setFocusable(false);
-        setShowGrid(true);
-
-        DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
-        centerRenderer.setHorizontalAlignment(SwingConstants.CENTER);
-        setDefaultRenderer(Object.class, centerRenderer);
-
-        DefaultTableCellRenderer leftRenderer = new DefaultTableCellRenderer();
-        leftRenderer.setHorizontalAlignment( SwingConstants.LEFT );
-        getColumnModel().getColumn(0).setCellRenderer(leftRenderer);
-        getColumnModel().getColumn(1).setCellRenderer(leftRenderer);
-
-        Action removeRow = new AbstractAction()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
-                int modelRow = Integer.parseInt( e.getActionCommand() );
-                model.removeRow(modelRow);
-            }
-        };
-
-        ButtonColumn buttonColumn = new ButtonColumn(this, model.getColumnCount()-1, new Color(199, 84, 80), additionalRemoveRowAction, removeRow);
-        buttonColumn.setMnemonic(KeyEvent.VK_D);
-        getColumnModel().getColumn( model.getColumnCount()-1).setMaxWidth(30);
-        getColumnModel().getColumn( model.getColumnCount()-1).setMinWidth(30);
-    }
-
-    public MultiTableModel getModel() {
-        return model;
-    }
-}
-
-class MultiTableModel extends DefaultTableModel {
-
-    Set<String> columnWithOptions;
-    List<Set<String>> rowOptions;
-
-    MultiTableModel() {
-        super();
-        rowOptions = new ArrayList<>();
-    }
-
-    public void setColumns(String[] columnNames, Set<String> columnWithOptions) {
-        setColumnIdentifiers(columnNames);
-        this.columnWithOptions = columnWithOptions;
-    }
-
-    public void addRow(String[] row, Set<String> options) {
-        super.addRow(row);
-        rowOptions.add(options);
-    }
-
-    @Override
-    public void removeRow(int row) {
-        super.removeRow(row);
-        rowOptions.remove(row);
-    }
-
-    public String[] getOptions(int row, int column) {
-        if (columnWithOptions.contains(getColumnName(column))) {
-            return rowOptions.get(row).toArray(new String[0]);
-        }
-        else {
-            return null;
-        }
+        table.addRow(row, responseOptions, responses);
     }
 }
